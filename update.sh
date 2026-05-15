20ff4bb09275d408ea1a121f23837ef57b0058d6
Dennis Baum <djaleksander@gmail.com>
2026-05-15 18:42:59 +0000

    fix: update.sh export GIT_HASH korrekt

--- update.sh
+++ update.sh
@@ -4,12 +4,9 @@ LOG=~/robot-core/update.log
 cd ~/robot-core
 
 echo "[update] Starte Update: $(date)" | tee -a "$LOG"
-
 git -c safe.directory=. pull --ff-only origin main 2>&1 | tee -a "$LOG"
 
-export GIT_HASH
-GIT_HASH=$(git -c safe.directory=. rev-parse HEAD)
+export GIT_HASH=$(git -c safe.directory=. rev-parse HEAD)
 echo "[update] Build startet (GIT_HASH=$GIT_HASH)..." | tee -a "$LOG"
-docker compose build --build-arg GIT_HASH="$GIT_HASH" robot-core 2>&1 | tail -3 | tee -a "$LOG"
-docker compose up -d robot-core 2>&1 | tail -3 | tee -a "$LOG"
+docker compose up -d --build robot-core 2>&1 | tail -4 | tee -a "$LOG"
 echo "[update] Abgeschlossen: $(date)" | tee -a "$LOG"
