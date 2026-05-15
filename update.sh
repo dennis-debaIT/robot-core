f2bbc44b994e3578ff5eab6edf47f67d0c5344c1
Dennis Baum <djaleksander@gmail.com>
2026-05-15 18:35:10 +0000

    fix: GIT_HASH export in update.sh; confirm dialog vor Install

--- update.sh
+++ update.sh
@@ -16,8 +16,9 @@ fi
 echo "[update] Update: $LOCAL -> $REMOTE" | tee -a "$LOG"
 git -c safe.directory=. pull --ff-only origin main 2>&1 | tee -a "$LOG"
 
+export GIT_HASH
 GIT_HASH=$(git -c safe.directory=. rev-parse HEAD)
-echo "[update] Build startet..." | tee -a "$LOG"
+echo "[update] Build startet (GIT_HASH=$GIT_HASH)..." | tee -a "$LOG"
 docker compose build --build-arg GIT_HASH="$GIT_HASH" robot-core 2>&1 | tail -3 | tee -a "$LOG"
 docker compose up -d robot-core 2>&1 | tail -3 | tee -a "$LOG"
 echo "[update] Abgeschlossen: $(date)" | tee -a "$LOG"
